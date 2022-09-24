c3186243ed2ef0856b6d199c11207462d5657c0f
NicolasOller <nicolasalejandrooller@gmail.com>
2022-09-24 02:08:07 -0300

    Se agrega curso de OpenBootcamp

---
+++ ejercicio2.js
@@ -0,0 +1,23 @@
+/* Ejercicio OpenBootcamp2
+
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const usuario = [ 
+    "Nicolas", 
+    25, 
+    true, 
+    new Date("nov 22 1996"), 
+    libro = {
+        titulo: "It", 
+        autor: "Stephen King", 
+        fecha: new Date("1986"), 
+        url: "https://es.wikipedia.org/wiki/It_(novela)"
+        } 
+    ]
+
+console.log(usuario)
---
+++ hola_mundo.js
@@ -0,0 +1,46 @@
+// console.log("Hola Mundo");
+
+// var x = 4
+// var y = 7
+
+// suma = x + y
+
+// console.log(x)
+// console.log(suma)
+
+
+//Ejemplo de condicional
+
+if (true){
+    console.log("Es correcto")
+}
+else {
+    console.log("No es correcto")
+};
+
+var variable="Bucle de diez mil lineas";
+
+for (i=0; i<10000; i++) {
+    console.log(variable)
+};
+
+x = 0;
+y = 1;
+c = null;
+z = undefined;
+h = 0;
+a = true;
+b = false;
+
+
+console.log(x+y);
+
+console.log(y==1);
+
+var variable = "Hola Mundo";
+
+for (i=0; i<10000; i++) {
+    variable="Soy una segunda variable"
+};
+
+console.log(variable);
